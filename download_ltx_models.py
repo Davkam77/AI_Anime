0b63982e80edfb71c20c1a8cb3a349b176d18345
Davkam77 <davit-marikyan@mail.ru>
2026-05-06 18:05:27 +0400

    1

--- download_ltx_models.py
+++ download_ltx_models.py
@@ -8,7 +8,6 @@ local_dir.mkdir(parents=True, exist_ok=True)
 snapshot_download(
     repo_id=repo_id,
     local_dir=str(local_dir),
-    local_dir_use_symlinks=False,
     allow_patterns=[
         "ltx-video-2b-v0.9.safetensors",
         "model_index.json",
